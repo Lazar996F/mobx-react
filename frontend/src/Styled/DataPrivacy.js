a3184e5085245942f6e35209c2c691868ffe5721
unknown <furtulal96@gmail.com>
2021-08-16 17:21:48 +0200

    formated some code

--- frontend/src/Styled/DataPrivacy.js
+++ frontend/src/Styled/DataPrivacy.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 
 export const Box = styled.section`
-    display: flex;
-  background-color: rgb(242,242,242);
-  width:100%;
+  display: flex;
+  background-color: rgb(242, 242, 242);
+  width: 100%;
+  padding-top: 20px;
 `
