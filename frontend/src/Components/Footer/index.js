12110091fe074c73b1e66e3951688ba42f6d7457
unknown <furtulal96@gmail.com>
2021-08-26 23:12:41 +0200

    footer content fixed

--- frontend/src/Components/Footer/index.js
+++ frontend/src/Components/Footer/index.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-`;
-
-const Text = styled.span`
-  font-size: 10px;
-  color: #A6A6A6;
+display: flex;
   position: absolute;
   bottom: 5px;
   right: 23px;
+  flex-direction: column;
+  font-size: 10px;
+  color: #A6A6A6;
 `;
 
 function Footer() {
   return (
-      <Text>&#169; 2019 Feller Privacy Terms of Service Content<br/>disclaimer Cookies</Text>
-  );
+    <Wrapper>
+      <span>&#169; 2019 Feller Privacy Terms of Service Content</span>
+      <span className="mr-auto">disclaimer Cookies</span>
+    </Wrapper>
+);
 }
 
 export default Footer;
